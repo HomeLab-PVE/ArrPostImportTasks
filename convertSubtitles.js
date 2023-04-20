494923343057742e6316eccf3f7ec6ecf5a216fe
rala-l <81008522+rala-l@users.noreply.github.com>
2023-04-20 17:03:36 +0300

    logger

--- convertSubtitles.js
+++ convertSubtitles.js
@@ -2,35 +2,40 @@ const fs = require('fs');
 const path = require('path');
 const languageEncoding = require("detect-file-encoding-and-language");
 const iconv = require('iconv-lite');
-const { fileExists } = require('./utils');
+const { fileExists, capitalize } = require('./utils');
+const { logger } = require('./logger');
 
 const detectEncoding = async (filePath) => {
 	try {
 		return await languageEncoding(filePath);
 	} catch (err) {
-		console.error("detectEncoding: ", err);
+		logger.error("detectEncoding: ", err);
 	}
 };
 
 const checkForExternalSubs = async (envs) => {
-	let arr = envs.movieFileRelativePath.split(".");
-	const extension = arr.pop();
-	const movieNameNoExtension = arr.join(".");
-	const subRips = ['srt', 'sub'];
-	for (let i = 0; i < subRips.length; i++) {
-		let filePath = path.join(envs.moviePath, `${movieNameNoExtension}.${subRips[i]}`);
-		if (await fileExists(filePath)) {
-			console.log(`Found external sub: ${filePath}`)
-			return {
-				filePath: filePath,
-				moviePath: envs.moviePath,
-				fileNameNoExtension: movieNameNoExtension,
-				extension: subRips[i],
-			};
+	try {
+		let arr = envs.movieFileRelativePath.split(".");
+		const extension = arr.pop();
+		const movieNameNoExtension = arr.join(".");
+		const subRips = ['srt', 'sub'];
+		for (let i = 0; i < subRips.length; i++) {
+			let filePath = path.join(envs.moviePath, `${movieNameNoExtension}.${subRips[i]}`);
+			if (await fileExists(filePath)) {
+				logger.info(`Found external sub: ${filePath}`)
+				return {
+					filePath: filePath,
+					moviePath: envs.moviePath,
+					fileNameNoExtension: movieNameNoExtension,
+					extension: subRips[i],
+				};
+			}
 		}
+		logger.info(`No external subs found in ${envs.moviePath}`)
+		return false;
+	} catch (err) {
+		logger.error("checkForExternalSubs: ", err);
 	}
-	console.log(`No external subs found in ${envs.moviePath}`)
-	return false;
 };
 
 const infoLanguageEncoding = (data) => {
@@ -38,8 +43,8 @@ const infoLanguageEncoding = (data) => {
 		return;
 	}
 	info = [];
-	info.push(`Detected language: ${data.language.toUpperCase()} with confidence ${data.confidence.language}`);
-	info.push(`Detected encoding: ${data.encoding} with confidence ${data.confidence.encoding}`);
+	info.push(`Detected language: ${capitalize(data.language)} with confidence: ${data.confidence.language}`);
+	info.push(`Detected encoding: ${data.encoding} with confidence: ${data.confidence.encoding}`);
 	return info;
 }
 
@@ -51,12 +56,12 @@ const convertSubtitles = async (filePath, originalEncoding, saveLocation) => {
 		const fileBuffer = await fs.promises.readFile(filePath);
 		const fileContent = iconv.decode(fileBuffer, originalEncoding);
 		const utf8Buffer = iconv.encode(fileContent, 'utf-8');
-		console.log(`Buffer converted from ${originalEncoding} to UTF-8 encoding`);
+		logger.info(`File buffer converted from ${originalEncoding} to UTF-8 encoding`);
 		await fs.promises.writeFile(saveLocation, utf8Buffer);
-		console.log(`File saved successfully in ${saveLocation}`);
+		logger.info(`File saved successfully in ${saveLocation}`);
 		return true;
 	} catch (err) {
-		console.error("convertSubtitles: ", err);
+		logger.error("convertSubtitles: ", err);
 	}
 }
 
@@ -67,10 +72,10 @@ const convertExternalSubtitles = async (opts = {}) => {
 			return;
 		}
 		if (detectedEnconding.language !== 'romanian') {
-			console.log('Detected language is not Romanian. Abort...');
+			logger.info('Detected language is not Romanian. Abort...');
 			return;
 		}
-		console.log(infoLanguageEncoding(detectedEnconding).join('\n'));
+		logger.info(infoLanguageEncoding(detectedEnconding).join('\n'));
 		
 		let saveLocation = path.join(opts.moviePath, `${opts.fileNameNoExtension}.ro.${opts.extension}`);
 		
@@ -79,7 +84,7 @@ const convertExternalSubtitles = async (opts = {}) => {
 		}
 		
 	} catch (err) {
-		console.error("convertExternalSubtitles: ", err);
+		logger.error("convertExternalSubtitles: ", err);
 	}
 };
 
@@ -87,24 +92,24 @@ const checkExtractedSubtitles = async (extractedSubtitles = []) => {
 	try {
 		for (let i = 0; i < extractedSubtitles.length; i++) {
 			let filePath = extractedSubtitles[i].path;
-			console.log(`Validating the extracted subtitle ${filePath}`);
+			logger.info(`Validating the extracted subtitle ${filePath}`);
 			let detectedEncondingAndLanguage = await detectEncoding(filePath);
-			console.log(infoLanguageEncoding(detectedEncondingAndLanguage).join('\n'));
+			logger.info(infoLanguageEncoding(detectedEncondingAndLanguage).join('\n'));
 			let encoding = detectedEncondingAndLanguage.encoding.toLowerCase();
 			if (encoding === 'utf-8' && detectedEncondingAndLanguage.language === 'romanian') {
-				console.log(`Everything is fine.`);
+				logger.info(`Everything is fine.`);
 				return;
 			} else {
 				if (detectedEncondingAndLanguage.language !== 'romanian') {
 					await fs.promises.unlink(filePath, {
 						force: true,
 					});
-					console.log(`Subtitle ${filePath} deleted because detected language is not Romanian.`);
+					logger.info(`Subtitle ${filePath} deleted because detected language is not Romanian.`);
 				}
 				if (encoding !== 'utf-8') {
-					console.log(`Subtitle neads to be converted to UTF-8`);
+					logger.info(`Subtitle neads to be converted to UTF-8`);
 					if (await convertSubtitles(filePath, detectedEncondingAndLanguage.encoding)) {
-						console.log(`Everything is fine now.`);
+						logger.info(`Everything is fine now.`);
 					}
 				}
 			}
@@ -113,7 +118,7 @@ const checkExtractedSubtitles = async (extractedSubtitles = []) => {
 		return;
 		
 	} catch (err) {
-		console.error("checkExtractedSubtitles: ", err);
+		logger.error("checkExtractedSubtitles: ", err);
 	}
 };
 	
