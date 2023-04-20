9ae7b5f1b682e54ac55ff72abf12b673962b4ca3
rala-l <81008522+rala-l@users.noreply.github.com>
2023-04-20 17:03:44 +0300

    logger

--- extractSubtitles.js
+++ extractSubtitles.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const { SubtitleParser } = require('matroska-subtitles');
 const { msToTime, fileExistsSync } = require('./utils');
+const { logger } = require('./logger');
 
 function searchSubsTracks(lang, array){
     for (let i=0; i < array.length; i++) {
@@ -13,69 +14,76 @@ function searchSubsTracks(lang, array){
 }
 
 const extractSubtitles = (mkvPath, outputDir) => new Promise((resolve, reject) => {
-	
-	const dir = outputDir || path.dirname(mkvPath);
-	const name = path.basename(mkvPath, path.extname(mkvPath));
-	console.log(`Searching for Romanian internal subtitle in ${mkvPath}`)
-	
-	const srtPath = function (language) {
-		const languageSuffix = language ? '.' + language : ''
-		return path.join(dir, name + languageSuffix + '.srt')
-	}
-
-	const tracks = new Map()
-	const subs = new SubtitleParser()
-
-	subs.once('tracks', tracks_ => {
-		const roTracks = [];
-		const roNames = ['rum', 'romanian', 'rom', 'ro'];
-		for (let i = 0; i < roNames.length; i++) {
-			let roTrack = searchSubsTracks(roNames[i], tracks_);
-			if (roTrack !== false) {
-				roTracks.push(roTrack);
-				break;
-			}
+	try {
+		const dir = outputDir || path.dirname(mkvPath);
+		const name = path.basename(mkvPath, path.extname(mkvPath));
+		logger.info(`Searching for Romanian internal subtitle in ${mkvPath}`)
+		
+		const srtPath = function (language) {
+			const languageSuffix = language ? '.' + language : ''
+			return path.join(dir, name + languageSuffix + '.srt')
 		}
-		if (roTracks.length === 0) {
-			console.log('No Romanian internal subtitles found. Aborting...');
-		}
-		roTracks.forEach(track => {
-			console.log(`Found Romanian internal subtitle in ${name} on track ${track.number}. Waiting for SUBRIP...`)
-			const language = 'rum';
-			let subtitlePath = srtPath(language);
-			for (let i = 2; fileExistsSync(subtitlePath); i++) {
-				subtitlePath = language ? srtPath(language + '.' + i) : srtPath(i)
+
+		const tracks = new Map()
+		const subs = new SubtitleParser()
+
+		subs.once('tracks', tracks_ => {
+			const roTracks = [];
+			const roNames = ['rum', 'romanian', 'rom', 'ro'];
+			for (let i = 0; i < roNames.length; i++) {
+				let roTrack = searchSubsTracks(roNames[i], tracks_);
+				if (roTrack !== false) {
+					roTracks.push(roTrack);
+					break;
+				}
 			}
-			tracks.set(track.number, {
-				index: 1,
-				file: fs.createWriteStream(subtitlePath),
-				language
+			if (roTracks.length === 0) {
+				logger.info('No Romanian internal subtitles found. Aborting...');
+			}
+			roTracks.forEach(track => {
+				logger.info(`Found Romanian internal subtitle in ${name} on track ${track.number}. Waiting for SUBRIP...`)
+				const language = 'rum';
+				let subtitlePath = srtPath(language);
+				for (let i = 2; fileExistsSync(subtitlePath); i++) {
+					subtitlePath = language ? srtPath(language + '.' + i) : srtPath(i)
+				}
+				tracks.set(track.number, {
+					index: 1,
+					file: fs.createWriteStream(subtitlePath),
+					language
+				})
 			})
 		})
-	})
 
-	subs.on('subtitle', (sub, trackNumber) => {
-		const track = tracks.get(trackNumber)
-		if (track) {
-			track.file.write(`${track.index++}\r\n`)
-			track.file.write(`${msToTime(sub.time)} --> ${msToTime(sub.time + sub.duration)}\r\n`)
-			track.file.write(`${sub.text}\r\n\r\n`)
-		} 
-	})
-
-	subs.on('finish', () => {
-		const tracks_ = []
-		tracks.forEach((track, i) => {
-			track.file.end()
-			tracks_.push({number: i, path: track.file.path, language: track.language})
-			console.log(`File saved successfully in ${track.file.path}`);
+		subs.on('subtitle', (sub, trackNumber) => {
+			const track = tracks.get(trackNumber)
+			if (track) {
+				track.file.write(`${track.index++}\r\n`)
+				track.file.write(`${msToTime(sub.time)} --> ${msToTime(sub.time + sub.duration)}\r\n`)
+				track.file.write(`${sub.text}\r\n\r\n`)
+			} 
 		})
-		resolve(tracks_)
-	})
 
-	const file = fs.createReadStream(mkvPath)
-	file.on('error', err => reject(err))
-	file.pipe(subs)
+		subs.on('finish', () => {
+			const tracks_ = []
+			tracks.forEach((track, i) => {
+				track.file.end()
+				tracks_.push({number: i, path: track.file.path, language: track.language})
+				logger.info(`File saved successfully in ${track.file.path}`);
+			})
+			resolve(tracks_)
+		})
+
+		const file = fs.createReadStream(mkvPath)
+		file.on('error', err => reject(err))
+		file.pipe(subs)
+		
+		return;
+		
+	} catch (err) {
+		logger.error("extractSubtitles: ", err);
+		resolve()
+	}
 })
 
 module.exports = extractSubtitles
