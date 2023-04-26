e28cb4f71495138402b9b9c08f31f84f8abdc552
rala-l <81008522+rala-l@users.noreply.github.com>
2023-04-26 18:32:13 +0300

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,56 @@
+const { logger } = require('./logger');
+const { fileExists } = require('./utils');
+const fs = require('fs');
+const path = require('path');
+const envs = require('./environments');
+
+const ignoreFile = async (action) => {
+	try {
+		if (!action) {
+			action = 'create';
+		}
+		const ignoreFilePath = path.join(path.dirname(envs.movieFilePath), '.ignore');
+		
+		if (action === 'create') {	
+			if (await fileExists(ignoreFilePath)) {
+				logger.info(`Ignore file exists: ${ignoreFilePath}`);
+			} else {
+				await fs.promises.writeFile(ignoreFilePath, "");
+				logger.info(`Ignore file created: ${ignoreFilePath}`);
+			}
+		} 
+		else if (action === 'delete') {
+			await fs.promises.unlink(ignoreFilePath, {
+				force: true,
+			});
+			logger.info(`Ignore file deleted: ${ignoreFilePath}`);
+		}
+	} catch (err) {
+		logger.error("ignoreFile: ", err);
+	}
+};
+
+const isRequierdEnvs = () => {
+	const missing = [];
+	for (let env in envs) {
+        if (envs[env] === null)
+			missing.push(env);
+    }
+	if (missing.length === 0 && envs.eventType === 'Download') {
+		return true;
+	} else {
+		if (missing.length !== 0) {
+			logger.warn(`Missing environments: ${missing.join(', ')}.`);
+		} else {
+			if (envs.eventType !== 'Download') {
+				logger.warn('Event type is not import.');
+			}
+		}
+	}
+	return false;
+};
+
+module.exports = {
+	isRequierdEnvs,
+	ignoreFile,
+}
