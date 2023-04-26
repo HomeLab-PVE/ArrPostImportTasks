600918dd254a2d4bf3276d89b231a8acd5b75cbe
rala-l <81008522+rala-l@users.noreply.github.com>
2023-04-26 16:02:53 +0300

    Update bazarr.js

--- services/bazarr.js
+++ services/bazarr.js
@@ -9,14 +9,15 @@ const bazarrRequest = async (path, opts = {}) => {
 			'x-api-key': `${envs.bazarrApiKey}`,
 		}
 		opts.maxAttempts = 3;
+		opts.retryDelay = 3000;
 		opts.fullResponse = true;
 		opts.json = true;
 
 		const res = await request(`${envs.bazarrAddress}/${path}`, opts);
 		if (res.statusCode < 200 || res.statusCode >= 300) {
 			throw new Error(`Status code: ${res.statusCode}, Status message: ${res.statusMessage}`);
-		} 
-
+		}
+		
 		return	[
 			res.body, 
 			res.statusCode
@@ -34,7 +35,7 @@ const bazarrSystemTasks = async (taskId) => {
 		const [ response, code ] = await bazarrRequest(
 			`api/system/tasks`, { 
 				method: 'POST',
-				body: { taskid: taskId},
+				body: { taskid: taskId , seriesid: '95'},
 			}
 		);
 		if (code === 204) {
@@ -49,22 +50,20 @@ const bazarrSystemTasks = async (taskId) => {
 	}
 };
 
-const bazarrSearchSubtitle = async (taskId) => {
+const bazarrSearchSubtitle = async ( opts = {} ) => {
 	try {
 		logger.info(`Trigger Bazarr subtitile index/search`);
-		const type = (envs.importArr === 'radarr') ? 'movies' : 'series';
-		const key = (type === 'movies') ? 'radarrid' : 'seriesid';
-		const searchId = (type === 'movies') ? envs.videoId : process.env.sonarr_series_id;
-		if (!searchId) {
+		const key = (opts.type === 'movies') ? 'radarrid' : 'seriesid';
+		if (!opts.searchId) {
 			logger.warn(`We cannot search for subtitles because search id is missing`);
 			return;
 		}
 		const [ response, code ] = await bazarrRequest(
-			`api/${type}`, { 
+			`api/${opts.type}`, { 
 				method: 'PATCH',
 				body: { 
 					action: 'search-missing',
-					[key]: searchId,
+					[key]: opts.searchId,
 				},
 			}
 		);
@@ -80,14 +79,59 @@ const bazarrSearchSubtitle = async (taskId) => {
 	}
 };
 
+const bazarrCheckSync = async () => {
+	try {
+		logger.info(`Waiting Bazarr to sync from ${capitalize(envs.importArr)}`);
+		const type = (envs.importArr === 'radarr') ? 'movies' : 'series';
+		const searchId = (type === 'movies') ? envs.videoId : process.env.sonarr_series_id;
+		if (!searchId) {
+			logger.warn(`We cannot wait for sync because search id is missing`);
+			return false;
+		}
+		
+		const data = {
+			type: type,
+			searchId: searchId,
+		};
+		
+		const path = (type === 'movies') ? `movies?radarrid[]=${searchId}` : `episodes?seriesid[]=${searchId}`;
+		
+		let awaitMs = 100;
+		for (let i = 0; i < awaitMs; i++) { 
+			await new Promise(r => setTimeout(r, 2500));
+			let [ response, code ] = await bazarrRequest(`api/${path}`);
+			if (code === 200 && type === 'movies') {
+				if (response.data && response.data.length > 0) {
+					return data;
+				}
+			} 
+			else if (code === 200 && type === 'series') {
+				let wanted = response.data.find(obj => obj.episode_file_id == envs.videoId);
+				if (wanted) {
+					return data;
+				}
+			}
+		}
+		
+		logger.warn(`Synchronization from ${capitalize(envs.importArr)} failed`);
+		
+		return false;
+		
+	} catch (err) {
+		logger.error("bazarrCheckSync: ", err);
+	}
+};
+
 const ruBazarrTasks = async () => {
 	try {
 		if (!envs.bazarrAddress || !envs.bazarrApiKey) {
 			logger.warn(`Bazarr IP:PORT/API Key not found in .env. Skiping Bazarr tasks...`)
 			return;
 		}
+		
 		if (envs.importArr === 'sonarr') await bazarrSystemTasks('sync_episodes');
-		await bazarrSearchSubtitle();
+		const syncData = await bazarrCheckSync();
+		if (syncData) await bazarrSearchSubtitle(syncData);
 		
 	} catch (err) {
 		logger.error("ruBazarrTasks: ", err);
