713bf9930094b014eeb83de187c3cee52fe63061
rala-l <81008522+rala-l@users.noreply.github.com>
2023-05-08 12:41:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { logger } = require('./logger');
-const { isRequierdEnvs } = require('./functions');
+const { isRequierdEnvs, ignoreFile } = require('./functions');
 const { runSubtitlesTasks } = require('./subtitles/subtitlesTasks');
 const { runBazarrTasks } = require('./services/bazarr');
 const { runJellyfinTasks } = require('./services/jellyfin');
@@ -8,8 +8,10 @@ if (!isRequierdEnvs()) process.exit();
 
 (async function () {
 	logger.info(`Starting post import tasks`);
+	await ignoreFile();
 	await runSubtitlesTasks();
 	await runBazarrTasks();
 	await runJellyfinTasks();
+	await ignoreFile('delete');
 	logger.info(`Finished post import tasks`);
 })()
